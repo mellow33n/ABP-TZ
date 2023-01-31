e11c4517fe46c378b2e11e10600c8c6d1b7ba969
Ihor Parfenenko <kramzeee@gmail.com>
2023-01-31 22:58:11 +0200

    add validation

--- abp_tz/src/components/Main/Form/Form.jsx
+++ abp_tz/src/components/Main/Form/Form.jsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 import { getVinFetch } from "../../Store/Reducer/vinSlice";
 
-
-
-
 function Form() {
   const dispatch = useDispatch();
+  const [error, setError] = useState(false);
+
+  const regExVin = /^([a-zA-Z0-9]){2,17}$/;
 
   function handleSumbit(event) {
     event.preventDefault();
+
     const vinValue = event.target.form[0].value;
-    dispatch(getVinFetch(vinValue));
-    event.target.form[0].value = '';
+
+    if (regExVin.test(vinValue)) {
+      dispatch(getVinFetch(vinValue));
+      event.target.form[0].value = "";
+    } else {
+      setError(true);
+    }
   }
 
   return (
     <form action="" className="main-top">
       <h3>Enter VIN</h3>
-      <input type="text" />
+      {error ? (
+        <>
+          <input type="text" className="main-top-input input-error" />
+          <p className="input-error-text">
+            {"VIN consists of 17 letters and numbers, no spaces"}
+          </p>
+        </>
+      ) : (
+        <input type="text" className="main-top-input" />
+      )}
+
       <button type="submit" onClick={handleSumbit}>
         Search
       </button>
@@ -26,4 +43,4 @@ function Form() {
   );
 }
 
-export default Form
+export default Form;

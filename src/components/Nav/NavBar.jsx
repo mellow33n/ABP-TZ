0ec4a9a1645e4b29495a690aa38ded3ecba2bb2f
Ihor Parfenenko <kramzeee@gmail.com>
2023-02-02 00:02:52 +0200

    start styling, create a colorsheme, create design blocks and complete nav

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,18 +1,22 @@
-
+import './navbar.scss'
 import { NavLink, Outlet } from "react-router-dom";
+import home_logo from "../../images/home.svg"
+import info_logo from "../../images/info.svg"
 
 function NavBar() {
   return (
     <>
     <nav className="nav">
       <div className="nav-item">
-        <NavLink to="/">
-          <button className="nav-item-btn">Homepage</button>
+        <NavLink to="/" className={"nav-item-link"}>
+          <img src={home_logo} alt="home" className='nav-logo'/>
+          <p className="nav-item-btn">Homepage</p>
         </NavLink>
       </div>
       <div className="nav-item">
-        <NavLink to="/variables">
-          <button className="nav-item-btn">Variables</button>
+        <NavLink to="/variables" className={"nav-item-link"}>
+          <img src={info_logo} alt="info" className='nav-logo'/>
+          <p className="nav-item-btn">Variables</p>
         </NavLink>
       </div>
     </nav>
